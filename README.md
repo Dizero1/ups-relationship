73d42109a5d442a7f631d7f35fcd774413847ad9
Dizero1662 <805631356@qq.com>
2023-04-26 19:35:08 +0800

    modified:   README.md 	modified:   btag.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ups-relationship
 本项目属于课程作业，形式为py爬虫  
 旨在通过切片的tag来追踪不同up的关系热度，更好的分析up关系网
-<details><summary>更好的</summary>磕cp:)</details>
+<details><summary>更好的</summary>磕cp : )</details>
--- btag.py
+++ btag.py
@@ -1,6 +1,7 @@
 import requests
 import urllib3
 import time
+import re
 from bs4 import BeautifulSoup
 
 urllib3.disable_warnings()
@@ -8,11 +9,16 @@ hd = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
 }
 BV = 'BV1xP411U79V'
-def get_tag(BV):
-    url = f"https://www.bilibili.com/video/{BV}/"
+def get_tag_byhtml(bvid):
+    url = f"https://www.bilibili.com/video/{bvid}/"
     r = requests.get(url, headers=hd, verify=False)
     r.raise_for_status()
-    soup = BeautifulSoup(r.text,'lxml')
-    alltag =soup.find_all('a',attrs={'class':'tag-link'})
-    tags = [tag.string.replace(' ','').replace('\n','') for tag in alltag if tag.string != None]
-    return tags
+    soup = BeautifulSoup(r.text, 'lxml')
+    alltag = soup.find_all('a', attrs={'class':'tag-link'})
+    return [tag.string.replace(' ','').replace('\n','') for tag in alltag if tag.string != None]
+
+def get_tagid(bvid):
+    url = f"https://api.bilibili.com/x/tag/archive/tags?bvid={bvid}"
+    r = requests.get(url, headers=hd, verify=False)
+    r.raise_for_status()
+    return [data['tag_id'] for data in r.json()['data']]
