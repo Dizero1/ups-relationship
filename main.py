f7903ab95d87eade21f9303377a2d52156db9cf7
Dizero1662 <805631356@qq.com>
2023-05-03 23:18:56 +0800

    json save

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from btag import *
+import json
 
 tid = 2885698
 tname = '小由'
+
 print(f'loading:list of {tname} (40 for 1 page)')
 vlist = [Video(bvid) for bvid in bvidlist_bytid(tid)]
 print('loading completed')
+data = pd.DataFrame([v.stat for v in vlist])
+data.to_json(f'test.json','records',force_ascii=False)
 num = 1
 num = int(input('which video:'))
 while num > 0:
